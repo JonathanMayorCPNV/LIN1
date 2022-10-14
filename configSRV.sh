3733f9d65737eb9f49138f338a8ce0e715bb5cb7
JonathanMayorCPNV <115776017+JonathanMayorCPNV@users.noreply.github.com>
2022-10-14 11:19:32 +0200

    Update configSRV.sh

--- configSRV.sh
+++ configSRV.sh
@@ -1,2 +1,23 @@
 HOSTNAME="SRV-LIN-02"
 sudo hostnamectl set-hostname $HOSTNAME
+
+sudo net_FILE="/etc/network/interfaces"
+cat <<EOM >$net_FILE
+
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+auto ens32
+iface ens32 inet static
+address 10.10.10.12
+netmask 255.255.255.0
+gateway 10.10.10.2
+
+EOM
